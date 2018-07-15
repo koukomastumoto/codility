db74b0f3551ea3198ce3408a46870064ec816fae
Kouko <kouko.matsumoto@gmail.com>
2018-07-15 21:11:30 +0900

    lesson3 TapeEquilibrium

---
+++ 3_ TapeEquilibrium.py
@@ -0,0 +1,11 @@
+# https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
+
+def solution(A):
+    diff_min = None
+    for i, a in enumerate(A):
+        if i == 0:
+            continue
+        diff = abs(sum(A[:i]) - sum(A[i:]))    
+        if diff_min == None or diff < diff_min:
+            diff_min = diff
+    print(diff_min)        
